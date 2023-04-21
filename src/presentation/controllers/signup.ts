c20c8a4ad979b30892a9dec7f64cbde0ea29aecc
Gabriel Torres <gaabrieltt7@gmail.com>
2023-04-21 11:21:11 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,9 @@
 import { MissingParamError } from '../../presentation/errors/missing-param-error'
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { type Controller } from '../../protocols/controller'
+import { type HttpRequest, type HttpResponse } from '../../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import { HttpResponse } from '../../protocols/http'
+import { type HttpResponse } from '../../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
